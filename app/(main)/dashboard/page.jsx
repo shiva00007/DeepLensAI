2e55f8f488bd02b1f5c70c928c7e0d4e994a931e
shiva00007 <shivas91919@gmail.com>
2025-01-04 19:19:52 +0530

    Budget Progress Features Done

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,12 +4,27 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import React from "react";
 import AccountCard from "./_components/AccountCard";
+import { getCurrentBudget } from "@/actions/budget";
+import BudgetProgress from "./_components/BudgetProgress";
 
 const DashboardPage = async () => {
   const accounts = await getUserAccounts();
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
   return (
-    <div className="px-5 my-5">
+    <div className="space-y-8">
       {/* {budgetProgress} */}
+      {defaultAccount && (
+        <BudgetProgress
+          initialBudget={budgetData?.budget}
+          currentExpenses={budgetData?.currentExpenses || 0}
+        />
+      )}
 
       {/* DashboardOverview */}
 
